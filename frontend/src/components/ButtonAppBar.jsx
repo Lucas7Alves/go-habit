d27db54e58d1c3c98b9387d657309481ae0e7cc9
Leon de França <leon.franca75@gmail.com>
2025-04-24 21:09:40 -0300

    feat: add login verification with Firebase in ButtonAppBar

--- frontend/src/components/ButtonAppBar.jsx
+++ frontend/src/components/ButtonAppBar.jsx
@@ -1,3 +1,5 @@
+// src/components/ButtonAppBar.jsx
+
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -9,10 +11,15 @@ import MenuItem from '@mui/material/MenuItem';
 import { Link } from 'react-router-dom';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase/firebaseConfig";
+import { signOut } from "firebase/auth";
 
 export default function ButtonAppBar() {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
+
+  const [user] = useAuthState(auth);
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -22,18 +29,34 @@ export default function ButtonAppBar() {
   };
 
   return (
-    <Box sx={{ width: '100%', m: 0, p: 0, marginBottom: '10vh'}}>
-      <AppBar position="static" sx={{ width: '100%', paddingLeft: '3vw', paddingRight: '3vw', backgroundColor: '#996AF9'}}>
+    <Box sx={{ width: '100%', m: 0, p: 0, marginBottom: '6vh' }}>
+      <AppBar
+        position="fixed"  
+        sx={{
+          width: '100%',
+          paddingLeft: '3vw',
+          paddingRight: '3vw',
+          backgroundColor: '#996AF9',
+          zIndex: 1100,  
+        }}
+      >
         <Toolbar>
-          {/* Título alinhado à esquerda */}
-  <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'left', fontWeight: 'bold'}}>GoHabit</Typography>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'left', fontWeight: 'bold' }}>
+            GoHabit
+          </Typography>
 
           <Button color="inherit" sx={{ '&:hover': { backgroundColor: 'rgba(109, 39, 175, 0.5)', color: 'inherit' } }} component={Link} to="/">Início</Button>
           <Button color="inherit" sx={{ '&:hover': { backgroundColor: 'rgba(109, 39, 175, 0.5)', color: 'inherit' } }} component={Link} to="/quem-somos">Quem Somos</Button>
           <Button color="inherit" sx={{ '&:hover': { backgroundColor: 'rgba(109, 39, 175, 0.5)', color: 'inherit' } }} component={Link} to="/nossa-solucao">Nossa Solução</Button>
 
-          {/* Botão "Mais" com menu */}
-          <Button color="inherit" sx={{ '&:hover': { backgroundColor: 'rgba(109, 39, 175, 0.5)', color: 'inherit' } }} onClick={handleClick} endIcon={<KeyboardArrowDownIcon />}>Mais</Button>
+          <Button
+            color="inherit"
+            sx={{ '&:hover': { backgroundColor: 'rgba(109, 39, 175, 0.5)', color: 'inherit' } }}
+            onClick={handleClick}
+            endIcon={<KeyboardArrowDownIcon />}
+          >
+            Mais
+          </Button>
           <Menu
             anchorEl={anchorEl}
             open={open}
@@ -41,19 +64,60 @@ export default function ButtonAppBar() {
             anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
             transformOrigin={{ vertical: 'top', horizontal: 'right' }}
           >
-            <MenuItem sx={{ '&:hover': { backgroundColor: 'rgba(109, 39, 175, 0.5)', color: 'inherit' } }} component={Link} to="/modelo-de-negocio" onClick={handleClose}>
+            <MenuItem
+              sx={{ '&:hover': { backgroundColor: 'rgba(109, 39, 175, 0.5)', color: 'inherit' } }}
+              component={Link}
+              to="/modelo-de-negocio"
+              onClick={handleClose}
+            >
               Modelo de Negócio
             </MenuItem>
-            <MenuItem sx={{ '&:hover': { backgroundColor: 'rgba(109, 39, 175, 0.5)', color: 'inherit' } }} component={Link} to="/etapas-de-desenvolvimento" onClick={handleClose}>
+            <MenuItem
+              sx={{ '&:hover': { backgroundColor: 'rgba(109, 39, 175, 0.5)', color: 'inherit' } }}
+              component={Link}
+              to="/etapas-de-desenvolvimento"
+              onClick={handleClose}
+            >
               Etapas de Desenvolvimento
             </MenuItem>
-            <MenuItem sx={{ '&:hover': { backgroundColor: 'rgba(109, 39, 175, 0.5)', color: 'inherit' } }} component={Link} to="/documentos" onClick={handleClose}>
+            <MenuItem
+              sx={{ '&:hover': { backgroundColor: 'rgba(109, 39, 175, 0.5)', color: 'inherit' } }}
+              component={Link}
+              to="/documentos"
+              onClick={handleClose}
+            >
               Documentos
             </MenuItem>
           </Menu>
 
-          <Button color="inherit" sx={{ '&:hover': { backgroundColor: 'rgba(109, 39, 175, 0.5)', color: 'inherit' } }} component={Link} to="/login">Login</Button>
-          <Button color="inherit" sx={{ '&:hover': { backgroundColor: 'rgba(109, 39, 175, 0.5)', color: 'inherit' } }} component={Link} to="/cadastrar-se">Cadastra-se</Button>
+          {user ? (
+            <Button
+              color="inherit"
+              onClick={() => signOut(auth)}
+              sx={{ '&:hover': { backgroundColor: 'rgba(109, 39, 175, 0.5)' } }}
+            >
+              Sair
+            </Button>
+          ) : (
+            <>
+              <Button
+                color="inherit"
+                sx={{ '&:hover': { backgroundColor: 'rgba(109, 39, 175, 0.5)', color: 'inherit' } }}
+                component={Link}
+                to="/login"
+              >
+                Login
+              </Button>
+              <Button
+                color="inherit"
+                sx={{ '&:hover': { backgroundColor: 'rgba(109, 39, 175, 0.5)', color: 'inherit' } }}
+                component={Link}
+                to="/cadastrar-se"
+              >
+                Cadastrar-se
+              </Button>
+            </>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
