bf630462b7268ab76351fced8c693a9dafb8f214
joaogldn <joaogald07@gmail.com>
2025-05-08 12:35:56 -0300

    interface da tela principal 1.0

--- mobile/app/src/main/java/com/souunit/gohabit/view/PrincipalSolo.java
+++ mobile/app/src/main/java/com/souunit/gohabit/view/PrincipalSolo.java
@@ -1,6 +1,7 @@
 package com.souunit.gohabit.view;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +18,20 @@ public class PrincipalSolo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_principal_solo);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Referenciando e atualizando os textos das tarefas
+        TextView task1 = findViewById(R.id.task1).findViewById(R.id.task_text);
+        TextView task2 = findViewById(R.id.task2).findViewById(R.id.task_text);
+        TextView task3 = findViewById(R.id.task3).findViewById(R.id.task_text);
+
+        task1.setText("Beber 2 litros de água");
+        task2.setText("Fazer 20 abdominais");
+        task3.setText("Fazer 50 polichinelos");
     }
 }
