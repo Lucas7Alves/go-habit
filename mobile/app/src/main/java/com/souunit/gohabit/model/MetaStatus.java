7279b0cf1b991c7e99013c977890d2f38a2e9b38
Lucas Alves <64313623+Lucas7Alves@users.noreply.github.com>
2025-05-19 15:46:05 -0300

    update: metastatus now has string

--- mobile/app/src/main/java/com/souunit/gohabit/model/MetaStatus.java
+++ mobile/app/src/main/java/com/souunit/gohabit/model/MetaStatus.java
@@ -1,5 +1,24 @@
 package com.souunit.gohabit.model;
 
 public enum MetaStatus {
-    COMPLETED, NOT_STARTED, PENDING
+    COMPLETED ("completed"),
+    NOT_STARTED("not_started"),
+    PENDING ("pending");
+
+    private final String value;
+
+    MetaStatus(String value) {
+        this.value = value;
+    }
+
+    public String getValue() { return value; }
+
+    public static MetaStatus fromValue(String value) {
+        for (MetaStatus status : values()) {
+            if (status.value.equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+        return null;
+    }
 }
